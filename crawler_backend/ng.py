201b580d910fb81a28c0dc09a0524ac9af47ca05
tpr <thejesh.p.r@oracle.com>
2022-09-29 10:46:39 +0530

    added ng

---
+++ crawler_backend/ng.py
@@ -0,0 +1,51 @@
+import json
+import logging
+
+from crawler_backend import WebClient, BaseParser
+
+logging.getLogger(__name__)
+
+
+def scrape(obj: BaseParser):
+    arg = json.loads(obj.site_conf.extra_data_json)
+    grp, typ = arg.get("grp"), arg.get("typ")
+    base_url = obj.site_conf.base_url
+    base_url = base_url.format(grp=grp, typ=typ)
+    data = WebClient.post_phjs(base_url, return_json=True)
+
+    posts = data.get("data").get("posts")[::-1]
+    logging.debug(posts)
+
+    for post in posts:
+
+        data = {
+            "caption": "{}\n{}".format(post.get("title"), post.get("url")),
+            "title": post.get("title"),
+            "nsfw": post.get("nsfw"),
+            "post_url": post.get("url"),
+            "content_type": post.get("type"),
+            "up_vote": post.get("upVoteCount"),
+            "down_vote": post.get("downVoteCount"),
+            "description": post.get("description"),
+            "comments_count": post.get("commentsCount"),
+        }
+
+        # check post type
+        if post["type"] == "Photo":
+            data["url"] = post.get("images").get("image700").get("url")
+            obj.create_task(
+                unique_key=post.get("id"),
+                name=post['title'],
+                url=data["url"]
+                # data=data
+            )
+
+        elif post["type"] == "Animated":
+            data["url"] = post.get("images").get("image460sv").get("url")
+            obj.create_task(
+                unique_key=post.get("id"),
+                name=post['title'],
+                url=data["url"]
+                # data=data
+            )
+
--- crawler_backend/scraper_config.py
+++ crawler_backend/scraper_config.py
@@ -5,6 +5,7 @@ from .mm import scrape as MM_Scraper
 from .d2 import scrape as D2_Scraper
 from .rdt import scrape as RDT_Scraper
 from .spny import scrape as SNPY_Scraper
+from .ng import scrape as NG_Scraper
 
 
 def get_scrapper(name):
@@ -16,6 +17,7 @@ def get_scrapper(name):
         d2=D2_Scraper,
         rdt=RDT_Scraper,
         spny=SNPY_Scraper,
+        ng=NG_Scraper,
     )
     if name in scrapers.keys():
         return scrapers.get(name)
