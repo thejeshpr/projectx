b502a14988556f404cea94178a5955d70360e8bd
tpr <thejesh.p.r@oracle.com>
2022-09-30 21:09:27 +0530

    removed logs

--- crawler_backend/crdko.py
+++ crawler_backend/crdko.py
@@ -5,12 +5,8 @@ from crawler_backend import WebClient, BaseParser
 
 
 def scrape(obj: BaseParser):
-    logging.info("Establishing connection to url")
     base_url = obj.site_conf.base_url
     res = WebClient.get(base_url)
-
-    logging.debug(f"status code:{res.status_code}")
-    logging.debug(f"response: {res.content}")
 
     links = res.html.xpath("/html/body/div[2]/div/div[1]/div/main/div[3]/div[1]/div[1]/div/div[*]/div[2]/h2/a")
 
