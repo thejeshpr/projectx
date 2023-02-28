cb75b184fdf4ba9d97671cc8671012c9284a0a09
tpr <thejesh.p.r@oracle.com>
2023-02-28 11:46:10 +0530

    added btr_prgmng

---
+++ crawler_backend/cb_btr_prgrmng.py
@@ -0,0 +1,30 @@
+import urllib
+
+from crawler_backend import WebClient, BaseParser
+
+
+def scrape(obj: BaseParser):
+    base_url = obj.site_conf.base_url
+    res = WebClient.get(base_url)
+    links = []
+
+    selector = "#root > div > div.l.c > div > div > main > div"
+
+    divs = res.html.find(selector, first=True)
+    for div in divs.find(".l"):
+        if div:
+            a = div.find("a", first=True)
+            if a:
+                link: str = a.attrs.get("href")
+                h2 = a.find("h2", first=True)
+
+                if all([h2, link, link not in links, link.startswith("/")]):
+                    title = h2.text.strip()
+
+                    links.append(link)
+                    print(title)
+                    obj.create_task(
+                        unique_key=link,
+                        name=title,
+                        url=urllib.parse.urljoin(base_url, link)
+                    )
