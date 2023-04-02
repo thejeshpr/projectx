ca76f4ca3c27163a53efe6aa85074a3cb241c9a6
tpr <thejesh.p.r@oracle.com>
2023-04-02 20:37:56 +0530

    updated files

--- projectx_site/settings.py
+++ projectx_site/settings.py
@@ -92,35 +92,6 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ.get('CUSTOM_DATABASE_URL')
-# DATABASE_URL = os.environ.get('CUSTOM_DATABASE_URL')
-# if DATABASE_URL:
-#     db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-#     DATABASES['default'].update(db_from_env)
-
-DATABASES = {
-    'default': {
-        'ENGINE': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-        # If connecting with SSL, include the section below, replacing the
-        # file paths as appropriate.
-        'OPTIONS': {
-            # 'sslmode': 'verify-full',
-            # 'sslrootcert': '/certs/ca.crt',
-            # Either sslcert and sslkey (below) or PASSWORD (above) is
-            # required.
-            # 'sslcert': '/certs/client.myprojectuser.crt',
-            # 'sslkey': '/certs/client.myprojectuser.key',
-            # If applicable
-            # 'options': '--cluster={routing-id}',
-        },
-    },
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
