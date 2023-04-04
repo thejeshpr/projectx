a38694fb59610c2ae6a4bbcbd78fea8321da5513
tpr <thejesh.p.r@oracle.com>
2023-04-04 13:45:15 +0530

    updated base url of tbhp-news

--- crawler_backend/cb_tbhp_news.py
+++ crawler_backend/cb_tbhp_news.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import urllib.parse
 
@@ -7,15 +8,18 @@ from crawler_backend import WebClient, BaseParser
 def scrape(obj: BaseParser):
     base_url = obj.site_conf.base_url
     res = WebClient.get(base_url)
+    extras = json.loads(obj.site_conf.extra_data_json)
+    article_base_url = extras.get("base_url", "")
 
     li_list = res.html.xpath('//*[@id="newslist"]/li[*]/div/div[2]/h2/a')
 
     for li in li_list[::-1]:
         a = li.find('a', first=True)
+        url = urllib.parse.urljoin(article_base_url, a.attrs.get('href'))
 
         obj.create_task(
-            unique_key=a.attrs.get('href'),
+            unique_key=url,
             name=a.attrs.get('title').strip(),
-            url=a.attrs.get('href')
+            url=url
         )
 
