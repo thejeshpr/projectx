a9a99d61fff8f8b72afc6e7c6d8035f3a228526a
tpr <thejesh.p.r@oracle.com>
2023-05-19 10:14:13 +0530

    added engdt

---
+++ crawler_backend/cb_engdt.py
@@ -0,0 +1,23 @@
+import json
+import logging
+import urllib.parse
+
+from crawler_backend import WebClient, BaseParser
+
+
+def scrape(obj: BaseParser):
+    base_url = obj.site_conf.base_url
+    soup = WebClient.get_bs(base_url)
+
+    # find h2
+    for h2 in soup.find_all('h2', {'class': "My(0)"}):
+        # find links
+        a = h2.find('a')
+        if a:
+            url = urllib.parse.urljoin(base_url, a.attrs.get('href'))
+            obj.create_task(
+                unique_key=url,
+                name=a.text.strip(),
+                url=url
+            )
+
