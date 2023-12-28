16f080e44d975a42839aa02f14bde604ef4863a2
tpr <thejesh.p.r@oracle.com>
2023-12-28 14:27:33 +0530

    updated gh trending

--- crawler_backend/cb_gh_trending.py
+++ crawler_backend/cb_gh_trending.py
@@ -1,29 +1,25 @@
-import logging
+import json
+import urllib
+from urllib.parse import urlparse, urljoin
+from uuid import uuid4
 
-from crawler_backend import BaseParser, WebClient
-
-logging.getLogger(__name__)
+from crawler_backend import WebClient, BaseParser
 
 
 def scrape(obj: BaseParser):
-    logging.info("Establishing connection to url")
-    res = WebClient.get(obj.site_conf.base_url)
+    base_url = obj.site_conf.base_url
+    soup = WebClient.get_bs(base_url)
+    extras = json.loads(obj.site_conf.extra_data_json)
 
-    logging.debug(f"status code:{res.status_code}")
-    data = res.json()
-    logging.debug(f"json data: {data}")
+    for article in soup.find_all('article', {"class": "Box-row"}):
+        a = article.find('a', {'class': "Link"})
+        p = article.find('p')
+        data = p.text.strip() if p else ""
 
-    tasks = []
 
-    for repo in data:
-        logging.debug(repo)
-        tasks.append(
-            dict(
-                name=repo.get("repositoryName"),
-                url=repo.get("url"),
-                unique_key=repo.get("repositoryName"),
-                data=repo.get("url")
-            )
+        obj.create_task(
+            unique_key=str(uuid4()),
+            name=a.text.strip(),
+            url=f"{extras.get('artcile_base_url')}{a.get('href')}",
+            data=data
         )
-    logging.debug(f"found {len(tasks)}")
-    return tasks
