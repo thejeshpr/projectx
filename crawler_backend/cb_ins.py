3a6d42b60f49de025fd0302dc68c723aa9d6412e
tpr <thejesh.p.r@oracle.com>
2023-12-29 11:21:03 +0530

    updated ins

--- crawler_backend/cb_ins.py
+++ crawler_backend/cb_ins.py
@@ -5,17 +5,21 @@ def scrape(obj: BaseParser):
     base_url = obj.site_conf.base_url
     res = WebClient.get(base_url)
     # extras = json.loads(obj.site_conf.extra_data_json)
+    print(res.json())
 
     for item in res.json().get('data').get('news_list'):
-        name = f"[{item.get('news_obj').get('source_name')}] - {item.get('news_obj').get('title')}"
-        url = item.get('news_obj').get('source_url')
-        url = url.replace("?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts", "")
-        data = item.get('news_obj').get('content')
+        news_obj = item.get('news_obj')
+        print(news_obj)
+        if news_obj:
+            name = f"[{news_obj.get('source_name')}] - {news_obj.get('title')}"
+            url = news_obj.get('source_url') or news_obj.get('bottom_panel_link')
+            # url = url.replace("?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts", "")
+            data = news_obj.get('content')
 
-        obj.create_task(
-            unique_key=item.get("hash_id"),
-            name=name,
-            url=url,
-            data=data
-        )
+            obj.create_task(
+                unique_key=item.get("hash_id"),
+                name=name,
+                url=url,
+                data=data
+            )
 
