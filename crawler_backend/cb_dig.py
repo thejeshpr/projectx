95cb1a008690b8654334ffdc4fde53e4aae907f5
tpr <thejesh.p.r@oracle.com>
2023-12-30 15:53:12 +0530

    added dig

---
+++ crawler_backend/cb_dig.py
@@ -0,0 +1,17 @@
+import json
+from urllib.parse import urljoin
+from crawler_backend import WebClient, BaseParser
+
+
+def scrape(obj: BaseParser):
+    base_url = obj.site_conf.base_url
+    soup = WebClient.get_bs(base_url)
+    # extras = json.loads(obj.site_conf.extra_data_json)
+
+    for div in soup.find_all('div', {'class': "dw-single-update dw-tax-name col-9"}):
+        obj.create_task(
+            unique_key=div.find('a').get('href'),
+            name=div.find('a').text.strip(),
+            url=div.find('a').get('href'),
+            data=div.find('p').text.strip()
+        )
