fcef619804a70895eae856fe3a9433b578d0ab1f
tpr <thejesh.p.r@oracle.com>
2024-03-04 17:35:02 +0530

    updated cnt

--- crawler_backend/cb_cnt.py
+++ crawler_backend/cb_cnt.py
@@ -8,7 +8,10 @@ def scrape(obj: BaseParser):
     soup = WebClient.get_bs(base_url)
     # extras = json.loads(obj.site_conf.extra_data_json)
 
-    for a in soup.find_all('a', {'class': "c-storiesLatest_story xl:u-col-2 lg:u-col-2 md:u-col-3 sm:u-col-2 u-flexbox-column"}):
+    # c-storiesNeonLatest_story xl:u-col-2 lg:u-col-2 md:u-col-2 sm:u-col-2 u-flexbox-column
+    # c-storiesLatest_story xl:u-col-2 lg:u-col-2 md:u-col-3 sm:u-col-2 u-flexbox-column
+
+    for a in soup.find_all('a', {'class': "c-storiesNeonLatest_story xl:u-col-2 lg:u-col-2 md:u-col-2 sm:u-col-2 u-flexbox-column"}):
         url = urljoin(base_url, a.get('href'))
         obj.create_task(
             unique_key=url,
