ee6cfaf15a3b3d25e379e673549cf00cf27229bc
tpr <thejesh.p.r@oracle.com>
2024-07-02 23:02:52 +0530

    updated cnet

--- crawler_backend/cb_cnt.py
+++ crawler_backend/cb_cnt.py
@@ -11,7 +11,7 @@ def scrape(obj: BaseParser):
     # c-storiesNeonLatest_story xl:u-col-2 lg:u-col-2 md:u-col-2 sm:u-col-2 u-flexbox-column
     # c-storiesLatest_story xl:u-col-2 lg:u-col-2 md:u-col-3 sm:u-col-2 u-flexbox-column
 
-    for a in soup.find_all('a', {'class': "c-storiesNeonLatest_story xl:u-col-2 lg:u-col-2 md:u-col-2 sm:u-col-2 u-flexbox-column"}):
+    for a in soup.find_all('a', {'class': "c-storiesNeonLatest_story md:u-col-2 sm:u-col-2 u-flexbox-column lg:u-col-2"}):
         url = urljoin(base_url, a.get('href'))
         obj.create_task(
             unique_key=url,
