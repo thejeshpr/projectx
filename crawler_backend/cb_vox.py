9e809167f87873bbd6b9beef524b3547306e35bb
tpr <thejesh.p.r@oracle.com>
2024-07-17 19:16:12 +0530

    updated vox

--- crawler_backend/cb_vox.py
+++ crawler_backend/cb_vox.py
@@ -8,30 +8,12 @@ def scrape(obj: BaseParser):
     base_url = obj.site_conf.base_url
     soup = WebClient.get_bs(base_url)
 
-    # find banner posts
-    for div in soup.find_all('div', {'class': "c-entry-box-base c-entry-box-hero"}):
-        # find links
-        a = div.find('a')
-        if a:
-            url = a.attrs.get('href')
-            h3 = div.find('h3', {'class': 'c-entry-box-base__headline'})
-
-            # find titles
-            if h3:
-                a = h3.find('a')
-                url = url
-                obj.create_task(
-                    unique_key=url,
-                    name=a.text.strip(),
-                    url=url
-                )
-
-    # find remaining posts
-    for h2 in soup.find_all('h2', {'class': 'c-entry-box--compact__title'}):
-        a = h2.find('a')
-        if a:
+    s = set()
+    for i, a in enumerate(soup.find_all('a', {"class": "qcd9z1"}), 1):
+        if a and a.text not in s:
+            s.add(a.text)
             obj.create_task(
-                unique_key=a.attrs.get('href'),
+                unique_key=a.get("href"),
                 name=a.text.strip(),
-                url=a.attrs.get('href')
+                url=urllib.parse.urljoin(base_url, a.get("href"))
             )
