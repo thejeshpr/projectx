2d6aa1e2f714794bb77b46e08686e6fe209a939a
tpr <thejesh.p.r@oracle.com>
2024-08-04 21:25:34 +0530

    added cb_nb

---
+++ crawler_backend/cb_nb.py
@@ -0,0 +1,35 @@
+import json
+import logging
+import time
+import urllib.parse
+
+from crawler_backend import WebClient, BaseParser
+
+
+def scrape(obj: BaseParser):
+    base_url = obj.site_conf.base_url
+    extras = json.loads(obj.site_conf.extra_data_json)
+    base_url = base_url.format(searchParam=extras["searchParam"])
+
+    res = WebClient.get(base_url)
+
+    if res.status_code == 200:
+        data = res.json()["data"]
+        for item in data:
+            logging.debug("processing item: {}".format(item))
+            availableFrom = int(str(item["availableFrom"])[:-3])
+            availableFrom = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(availableFrom))
+            data = (f"plotArea: {item['plotArea']}\n"
+                    f"pricePerUnit: {item['pricePerUnit']}\n"
+                    f"completeStreetName: {item['completeStreetName']}\n"
+                    f"address: {item['address']}\n"
+                    f"location: {item['location']}\n"
+                    f"availableFrom: {availableFrom}\n")
+            obj.create_task(
+                unique_key=item["id"],
+                name=f'{item["propertyTitle"]}-{item["formattedPrice"]}',
+                url=item["shortUrl"],
+                data=data
+            )
+    else:
+        logging.error(f"failed to scrape item: cb_nb: {res.content}")
