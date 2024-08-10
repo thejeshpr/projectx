9c2b32b2e893b62e082c315e62cdb7be885675c2
tpr <thejesh.p.r@oracle.com>
2024-08-10 16:08:20 +0530

    updated bpanda

--- crawler_backend/cb_bpanda.py
+++ crawler_backend/cb_bpanda.py
@@ -9,23 +9,16 @@ def scrape(obj: BaseParser):
     typ = extras.get("type", "")
     url = obj.site_conf.base_url.format(category=cat, type=typ)
 
-    res = WebClient.get(url)
-    # links = res.html.xpath("/html/body/main/section/article[*]/h2/a")
-
-    section = res.html.find("section")
+    soup = WebClient.get_bs(url)
+    section = soup.find("section")
     if section:
-        section = section[0]
 
-        for intro_div, header in zip(section.find('div.intro'), section.find('header')):
-            a = header.find('a')[0]
+        for a in section.find_all("a", {"class": "title post-link"}):
             link = a.attrs.get("href").split("?")[0]
-            name = a.attrs.get("title").replace("Permanent Link to ", "")
-            p = intro_div.find('p', first=True)
-            data = p.text.strip() if p else ""
-
+            name = a.text.strip()
             obj.create_task(
                 unique_key=link,
                 name=name,
-                url=link,
-                data=data
+                url=link
+                # data=data
             )
