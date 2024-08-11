32bdf59dc93f37d915876b5c3a0115e28fbaf8df
tpr <thejesh.p.r@oracle.com>
2024-08-11 13:35:42 +0530

    fixed tcrunch

--- crawler_backend/cb_tcrunch.py
+++ crawler_backend/cb_tcrunch.py
@@ -8,11 +8,15 @@ def scrape(obj: BaseParser):
     base_url = obj.site_conf.base_url
     soup = WebClient.get_bs(base_url)
 
-    for a in soup.find_all('a', {'class': 'post-block__title__link'}):
-        url = a.attrs.get('href')
-        obj.create_task(
-            unique_key=url,
-            name=a.text.strip(),
-            url=url
-        )
+    for div in soup.find_all('div', {'class': 'wp-block-tc23-post-picker'}):
+        h2 = div.find("h2")
+        if h2:
+            a = h2.find("a")
+            if a:
+                url = a.attrs.get('href')
+                obj.create_task(
+                    unique_key=url,
+                    name=a.text.strip(),
+                    url=url
+                )
 
