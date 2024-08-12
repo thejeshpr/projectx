5162776e2f37df2e5c58ea379b7bc63de491e463
Thejesh <thejeshpr@gmail.com>
2024-08-12 11:45:04 +0000

    updated crckd

--- crawler_backend/cb_crckd.py
+++ crawler_backend/cb_crckd.py
@@ -7,15 +7,13 @@ from crawler_backend import WebClient, BaseParser
 def scrape(obj: BaseParser):
     base_url = obj.site_conf.base_url
     soup = WebClient.get_bs(base_url)
-
-    for li in soup.find("div", {"class": "flex-1 min-h-0"}).find_all("li"):
-        a = li.find("a")
-        h2 = li.find("h2")
-        p = li.find("p")
-        if a and h2:
+    
+    articles = soup.find_all("article")
+    for article in articles:
+        a = article.find("a")
+        if a:
             obj.create_task(
                 unique_key=a.attrs.get('href'),
-                name=h2.text.strip(),
-                url=a.attrs.get('href'),
-                data=p.text.strip() if p else ""
+                name=a.attrs.get('title'),
+                url=a.attrs.get('href')
             )
